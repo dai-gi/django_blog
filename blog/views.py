ba3dcc6d06db00e4d858cf01c53caec0a3ff00db
daisuke524 <daisuke.u524@gmail.com>
2020-04-09 20:50:09 +0900

    テンプレート追加まで完了

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.utils import timezone
+from blog.models import Post
+from django.views.generic import (ListView)
 
-# Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
