c87117ca0d9a7b0a9509ead01a9c6776ed44c74f
daisuke524 <daisuke.u524@gmail.com>
2020-04-16 19:41:10 +0900

    詳細ページの Viewを追加

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.utils import timezone
 from blog.models import Post
-from django.views.generic import (ListView)
+from django.views.generic import (ListView, DetailView)
 
 
 class PostListView(ListView):
@@ -9,3 +9,7 @@ class PostListView(ListView):
 
     def get_queryset(self):
         return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
