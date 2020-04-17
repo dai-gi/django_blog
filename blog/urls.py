370877a91b61ae62c5f43e5e5636146388be5c85
daisuke524 <daisuke.u524@gmail.com>
2020-04-17 23:32:03 +0900

    公開ボタンのurlを作成

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,8 @@ from blog import views
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', views.CreatePostView.as_view(), name='post_new'),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
+    path('drafts/', views.DraftListView.as_view(), name='post_draft_list'),
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
 ]
