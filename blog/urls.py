d69a606bfe2f35fa31cf520250009f3c72292732
daisuke524 <daisuke.u524@gmail.com>
2020-04-20 15:46:27 +0900

    削除ボタンのurlを作成

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,12 @@
-from django.urls import path
 from blog import views
+from django.urls import path
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
-    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
-    path('post/new/', views.CreatePostView.as_view(), name='post_new'),
-    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
     path('drafts/', views.DraftListView.as_view(), name='post_draft_list'),
+    path('post/new/', views.CreatePostView.as_view(), name='post_new'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
     path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
     path('post/<int:pk>/remove/', views.PostDeleteView.as_view(), name='post_remove'),
 ]
