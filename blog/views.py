507ca28d34807da63f93782973ee5008525cab40
daisuke524 <daisuke.u524@gmail.com>
2020-04-22 01:41:19 +0900

    コメントのViewを作成

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.utils import timezone
 from blog.models import Post
-from blog.forms import PostForm
+from blog.forms import PostForm, CommentForm
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import (TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView)
 
@@ -52,3 +52,16 @@ class PostDeleteView(LoginRequiredMixin, DeleteView):
 	template_name = "blog/post_confirm_delete.html"
 	success_url = reverse_lazy('post_list')
 
+@login_required
+def post_comment(request,pk):
+	post = get_object_or_404(Post, pk=pk)
+	if request.method == "POST":
+		form = CommentForm(request.POST)
+		if form.is_valid():
+			comment = form.save(commit=False)
+			comment.post = post
+			comment.save()
+			return redirect('post_detail', pk=post.pk)
+	else:
+		form = CommentForm()
+	return render(request, 'blog/post_comment.html', {'form': form})
