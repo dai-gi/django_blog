8e93edfdb4e7eb8138c6e4987b53b98d880131ff
daisuke524 <daisuke.u524@gmail.com>
2020-04-28 19:41:31 +0900

    Postモデルに追加したCategoryをコメントアウト、その他変更

--- blog/models.py
+++ blog/models.py
@@ -5,12 +5,11 @@ from django.urls import reverse
 
 
 class Post(models.Model):
-    author = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='images', blank=True, null=True)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    # category = models.ForeignKey(Category, on_delete=models.PROTECT)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
@@ -18,14 +17,14 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
     def get_absolute_url(self):
         return reverse("post_detail", kwargs={'pk': self.pk})
 
     def __str__(self):
         return self.title
-
-    def approved_comments(self):
-        return self.comments.filter(approved_comment=True)
 
 
 class Comment(models.Model):
