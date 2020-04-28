9f36aa5aec38de786fde775ec0d05ff0bda0a4fb
daisuke524 <daisuke.u524@gmail.com>
2020-04-28 09:44:12 +0900

    Categoryモデルを紐づけ

--- blog/models.py
+++ blog/models.py
@@ -5,45 +5,51 @@ from django.urls import reverse
 
 
 class Post(models.Model):
-	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-	image = models.ImageField(upload_to='images',blank=True, null=True)
-	title = models.CharField(max_length=200)
-	text = models.TextField()
-	created_date = models.DateTimeField(default=timezone.now)
-	published_date = models.DateTimeField(blank=True, null=True)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images', blank=True, null=True)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
 
-	def publish(self):
-		self.published_date = timezone.now()
-		self.save()
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
 
-	def get_absolute_url(self):
-		return reverse("post_detail", kwargs={'pk':self.pk})
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={'pk': self.pk})
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
 
-	def approved_comments(self):
-		return self.comments.filter(approved_comment=True)
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
 
 class Comment(models.Model):
-	post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
-	author = models.CharField(max_length=200)
-	text = models.TextField()
-	created_date = models.DateTimeField(default=timezone.now)
-	approved_comment = models.BooleanField(default=False)
+    post = models.ForeignKey(
+        'blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
 
-	def approve(self):
-		self.approved_comment = True
-		self.save()
+    def approve(self):
+        self.approved_comment = True
+        self.save()
 
-	def get_absolute_url(self):
-		return reverse("post_list")
+    def get_absolute_url(self):
+        return reverse("post_list")
 
-	def __str__(self):
-		return self.text
+    def __str__(self):
+        return self.text
 # Create your models here.
-class Category(models.Model): 
-	name = models.CharField(max_length=50)
 
-	def __str__(self):
-		return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
