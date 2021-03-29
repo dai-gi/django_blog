6823cf430e5d7cafd87405657e12e61e158ef7f6
dai-gi <daisuke.u524@gmail.com>
2021-03-29 22:51:37 +0900

    新規投稿用のURLを追加

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from blog import views
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'), 
+    path('post/new/', views.CreatePostView.as_view(), name='post_new'), 
 ]
