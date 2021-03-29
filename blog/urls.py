b18051f7087c34f6e205d06ac32d3ff77e04be86
dai-gi <daisuke.u524@gmail.com>
2021-03-29 21:21:03 +0900

    詳細画面のURLを追加

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index')
+    path('', views.IndexView.as_view(), name='index'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
 ]
